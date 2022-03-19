4134e33468f268bfff8f54ec6dc9f8df1a392a2b
Gaurav Joshi <gaurav.joshi@utdallas.edu>
2022-03-18 22:33:12 -0500

    Completed Mini Project 3

--- Mini Project 3/question1.R
+++ Mini Project 3/question1.R
@@ -1,6 +1,6 @@
 
 # Question 1b
-mse <- function(n, theta) {
+generate_mom_mle <- function(n, theta) {
   # Create uniform distribution between 0 to theta
   generate_uniform_dist <- runif(n, min=0, max=theta)
   
@@ -13,14 +13,67 @@ mse <- function(n, theta) {
 
 mse_est <- function(n, theta) {
   est <- replicate(1000, mse(n, theta))
-  est <- (est - theta)^2
-  est.methodOfMoments <- est[c(FALSE, TRUE)]
-  est.maxLikelihoodEst <- est[c(TRUE, FALSE)]
   
-  return(c(mean(est.methodOfMoments), mean(est.maxLikelihoodEst)))
+  # calculate mean squared err
+  est <- (est - theta)^2
+  
+  methodOfMoments <- est[c(FALSE, TRUE)]
+  maxLikelihoodEst <- est[c(TRUE, FALSE)]
+  
+  # store mean of mom and mle
+  mean_of_mom <- mean(methodOfMoments)
+  mean_of_mle <- mean(maxLikelihoodEst)
+  
+  return(c(mean_of_mom, mean_of_mle))
 }
 
-mean_sq_err_1_1 <- mse_est(1, 1)
+mean_sq_err <- function(mse_est, n, theta) {
+  return(mse_est(n, theta))
+}
 
 # Question 1c
 
+# create variables to store results for different values to theta
+one_one <- mean_sq_err(mse_est, 1, 1)
+one_five <- mean_sq_err(mse_est, 1, 5)
+one_fifty <- mean_sq_err(mse_est, 1, 50)
+one_hundr <- mean_sq_err(mse_est, 1, 100)
+
+two_one <- mean_sq_err(mse_est, 2, 1)
+two_five <- mean_sq_err(mse_est, 2, 5)
+two_fifty <- mean_sq_err(mse_est, 2, 50)
+two_hundr <- mean_sq_err(mse_est, 2, 100)
+
+three_one <- mean_sq_err(mse_est, 3, 1)
+three_five <- mean_sq_err(mse_est, 3, 5)
+three_fifty <- mean_sq_err(mse_est, 3, 50)
+three_hundr <- mean_sq_err(mse_est, 3, 100)
+
+five_one <- mean_sq_err(mse_est, 5, 1)
+five_five <- mean_sq_err(mse_est, 5, 5)
+five_fifty <- mean_sq_err(mse_est, 5, 50)
+five_hundr <- mean_sq_err(mse_est, 5, 100)
+
+ten_one <- mean_sq_err(mse_est, 10, 1)
+ten_five <- mean_sq_err(mse_est, 10, 5)
+ten_fifty <- mean_sq_err(mse_est, 10, 50)
+ten_hundr <- mean_sq_err(mse_est, 10, 100)
+
+thirty_one <- mean_sq_err(mse_est, 30, 1)
+thirty_five <- mean_sq_err(mse_est, 30, 5)
+thirty_fifty <- mean_sq_err(mse_est, 30, 50)
+thirty_hundr <- mean_sq_err(mse_est, 30, 100)
+
+# create vectors from inputs, mle and mse 
+input_vals_for_theta <- c(1, 5, 50, 100)
+input_vals_for_N <- c(1, 2, 3, 5, 10, 30)
+
+mom_vals <- c(one_one[1], two_one[1], three_one[1], five_one[1], ten_one[1], thirty_one[1])
+mle_vals <- c(one_one[2], two_one[2], three_one[2], five_one[2], ten_one[2], thirty_one[2])
+
+textColor <- c("blue", "red")
+
+# plot graph mse when n = 1, theta = {}
+plot(input_vals_for_N, mom_vals, main = "Graph when Theta = 1", type="b", xlab="theta", ylab="Mean Squared Error", col="blue")
+lines(input_vals_for_N, mle_vals, type="b", col="red")
+legend("topright", legend = c("MOM", "MLE"), col = textColor, text.col = textColor, ncol = 1, lty = 1)
--- Mini Project 3/question2.R
+++ Mini Project 3/question2.R
@@ -1,17 +1,16 @@
 
 # question 2c
-log_max_likelihood <- function(par, datum) {
-  res <- length(datum) * log(par) - (par + 1) * sum(log(datum))
-  
+log_max_likelihood <- function(theta, dat) {
+  res <- length(dat) * log(theta) - (theta + 1) * sum(log(dat))
   return (-res)
 }
 
 input <- c(21.42, 14.65, 50.42, 28.78, 11.23)
 
-max_likelihood_est <- optim(par = 0.926, fn = log_max_likelihood, method = "L-BFGS-B", hessian = TRUE, lower = 0.01, dat = input)
+max_likelihood_est <- optim(par=1, fn = log_max_likelihood, method = "L-BFGS-B", hessian = TRUE, lower = 0.01, dat = input)
 
 # question 2d
-hessian_val <- max_likelihood_est$hessian
+hessian_val <- max_likelihood_est$hessian[1]
 standard_error <- (1/hessian_val)^0.5
 
 #confidence interval
