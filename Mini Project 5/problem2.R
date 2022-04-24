bdf0683f4a9a23abf4d28844eeffb46133856a6e
Gaurav Joshi <gaurav.joshi@utdallas.edu>
2022-04-23 20:46:39 -0500

    Completed Project 5

--- Mini Project 5/problem2.R
+++ Mini Project 5/problem2.R
@@ -58,7 +58,9 @@ bci <- matrix(c(b_proportion(5, 0.01), b_proportion(10, 0.01),  b_proportion(30,
                        b_proportion(5, 10), b_proportion(10, 10), b_proportion(30, 10), b_proportion(100, 10)), nrow = 4, ncol = 4)
 
 par(mfrow=c(2,2))
-plot(c(5, 10, 30, 100), zci[, 1], xlab = 'n', ylab = 'proportion', main = "L = 0.01", xlim = c(1, 100), ylim = c(0, 1), type = 'b', col = 'red')
+plot(c(5, 10, 30, 100), zci[, 1], xlab = 'n', ylab = 'proportion', 
+     main = "L = 0.01", xlim = c(1, 100), ylim = c(0, 1), 
+     type = 'b', col = 'red')
 lines(c(5, 10, 30, 100), bci[, 1], type = 'b', col = 'blue')
 
 plot(c(5, 10, 30, 100), zci[, 2], xlab = 'n', ylab = 'proportion', main = "L = 0.1", xlim = c(1, 100), ylim = c(0, 1), type = 'b', col = 'red')
@@ -70,17 +72,19 @@ lines(c(5, 10, 30, 100), bci[, 3], type = 'b', col = 'blue')
 plot(c(5, 10, 30, 100), zci[, 4], xlab = 'n', ylab = 'proportion', main = "L = 10", xlim = c(1, 100), ylim = c(0, 1), type = 'b', col = 'red')
 lines(c(5, 10, 30, 100), bci[, 4], type = 'b', col = 'blue')
 
-plot(c(5, 10, 30, 100), zci[1,], xlab = 'n', ylab = 'proportion', main = "N = 5", xlim = c(1, 100), ylim = c(0, 1), type = 'b', col = 'red')
-lines(c(5, 10, 30, 100), bci[1,], type = 'b', col = 'blue')
+plot(c(0.01, 0.1, 1, 10), zci[1,], xlab = 'lambda', ylab = 'proportion', 
+     main = "N = 5", xlim = c(0.01, 10), ylim = c(0, 1), 
+     type = 'b', col = 'red')
+lines(c(0.01, 0.1, 1, 10), bci[1,], type = 'b', col = 'blue')
 
-plot(c(5, 10, 30, 100), zci[2,], xlab = 'n', ylab = 'proportion', main = "N = 10", xlim = c(1, 100), ylim = c(0, 1), type = 'b', col = 'red')
-lines(c(5, 10, 30, 100), bci[2,], type = 'b', col = 'blue')
+plot(c(0.01, 0.1, 1, 10), zci[2,], xlab = 'lambda', ylab = 'proportion', main = "N = 10", xlim = c(0.01, 10), ylim = c(0, 1), type = 'b', col = 'red')
+lines(c(0.01, 0.1, 1, 10), bci[2,], type = 'b', col = 'blue')
 
-plot(c(5, 10, 30, 100), zci[3,], xlab = 'n', ylab = 'proportion', main = "N = 30", xlim = c(1, 100), ylim = c(0, 1), type = 'b', col = 'red')
-lines(c(5, 10, 30, 100), bci[3,], type = 'b', col = 'blue')
+plot(c(0.01, 0.1, 1, 10), zci[3,], xlab = 'lambda', ylab = 'proportion', main = "N = 30", xlim = c(0.01, 10), ylim = c(0, 1), type = 'b', col = 'red')
+lines(c(0.01, 0.1, 1, 10), bci[3,], type = 'b', col = 'blue')
 
-plot(c(5, 10, 30, 100), zci[4,], xlab = 'n', ylab = 'proportion', main = "N = 100", xlim = c(1, 100), ylim = c(0, 1), type = 'b', col = 'red')
-lines(c(5, 10, 30, 100), bci[4,], type = 'b', col = 'blue')
+plot(c(0.01, 0.1, 1, 10), zci[4,], xlab = 'lambda', ylab = 'proportion', main = "N = 100", xlim = c(0.01, 10), ylim = c(0, 1), type = 'b', col = 'red')
+lines(c(0.01, 0.1, 1, 10), bci[4,], type = 'b', col = 'blue')
 
 
 
